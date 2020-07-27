ff16e4d3b9a4aeeaabdbe73dc0e952dc8388437b
DANIELSULUB <DANIELSULUB@SISTEMASDS>
2020-07-27 15:12:17 -0500

    Ok segunda modificacion

--- DXWebApplication1/DXApplication1/Form1.Designer.cs
+++ DXWebApplication1/DXApplication1/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(399, 37);
+            this.button1.Location = new System.Drawing.Point(398, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 66);
             this.button1.TabIndex = 1;
